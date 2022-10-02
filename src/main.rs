50002e378527f89b5eee4ff494260f249a5460c8
acm022 <Michael.babic@haw-hamburg.de>
2022-10-02 23:50:18 +0200

    implement start menu detection

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,16 @@ mod taskbar;
 mod tb_settings;
 mod windows_calls;
 fn main() {
+    windows_calls::initialize_windows_calls();
     let taskbar = Taskbar::new();
     let sleep_in_ms = time::Duration::from_millis(tb_settings::get_sleep_time_in_ms());
 
     loop {
         thread::sleep(sleep_in_ms);
         let is_hovering = taskbar.is_hovering_on_tb();
+        let start_menu_open = windows_calls::get_start_myenu_open();
         println!("{is_hovering}");
+        println!("{start_menu_open}");
+        println!("------------------");
     }
 }
