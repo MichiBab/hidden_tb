4a6ab1d8ad9932145625af11ca8cc3dfd944110c
acm022 <michael.babic@haw-hamburg.de>
2022-10-05 22:40:53 +0200

    improve loop

--- src/main.rs
+++ src/main.rs
@@ -14,22 +14,25 @@ fn main() {
     let settings = tb_settings::get_tb_settings();
     let dur = time::Duration::from_millis(settings.get_sleep_time_in_ms());
     let mut taskbar = Taskbar::new();
+    // wait until all handles are available
+    while taskbar.contains_none() {
+        taskbar.refresh_handles();
+        thread::sleep(dur);
+        continue;
+    }
+
+    //spawn system tray icon
     let ui_handle = std::thread::spawn(|| -> () {
         tray::start_tray_icon();
     });
+
     let mut infrequent_counter: usize = 0;
     loop {
         if signaling::get_exit_called() {
             break;
         }
-        thread::sleep(dur);
-        if taskbar.contains_none() {
-            taskbar.refresh_handles();
-            continue;
-        }
 
         infrequent_counter %= settings.get_infrequent_count();
-
         if infrequent_counter == 0 {
             if settings.get_autohide() {
                 taskbar.check_and_set_taskbar_transparency_state();
